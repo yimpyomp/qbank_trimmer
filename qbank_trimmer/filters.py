17f03e7415ee42fa730ede9d5d8bd8b51b87921b
yimpyomp <sebastian.enzi@gmail.com>
2026-06-29 11:07:47 -0500

    remove unused functions

--- qbank_trimmer/filters.py
+++ qbank_trimmer/filters.py
@@ -7,54 +7,6 @@ def normalize_filter_text(text):
     return " ".join(text.split()).lower()
 
 
-def filter_learning_areas(learning_area_filter, catalog):
-    """
-    Does what it says it does
-    :param learning_area_filter: List of learning areas to be targeted
-    :param catalog: Big ol json
-    :return: dictionary of filtered stuff
-    """
-    trimmed_catalog = {}
-    for entry in catalog.keys():
-        question = catalog[entry]
-        for item in learning_area_filter:
-            if item in question:
-                trimmed_catalog[entry] = question
-    return trimmed_catalog
-
-
-def filter_skills(skill_filter, catalog):
-    """
-    Like learning area filter but for skills
-    :param skill_filter:
-    :param catalog:
-    :return:
-    """
-    trimmed_catalog = {}
-    for entry in catalog.keys():
-        question = catalog[entry]
-        for item in skill_filter:
-            if item in question:
-                trimmed_catalog[entry] = question
-    return trimmed_catalog
-
-
-def filter_difficulty(difficulty_filter, catalog):
-    """
-    take a wild guess
-    :param difficulty_filter:
-    :param catalog:
-    :return:
-    """
-    trimmed_catalog = {}
-    for entry in catalog.keys():
-        question = catalog[entry]
-        for item in difficulty_filter:
-            if item in question:
-                trimmed_catalog[entry] = question
-    return trimmed_catalog
-
-
 def filter_catalog(catalog, learning_area=None, skill=None, difficulty=None):
     # New dictionary to store results
     filtered_catalog = {}
