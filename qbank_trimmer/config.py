a477839eb5fd9037218013e8235258525d2daf36
yimpyomp <sebastian.enzi@gmail.com>
2026-06-29 11:35:01 -0500

    basic changes to add functionality with math sections

--- qbank_trimmer/config.py
+++ qbank_trimmer/config.py
@@ -1,16 +1,21 @@
 from pathlib import Path
 
-BANK_DIR = Path("bank")
-SOLUTIONS_DIR = Path("solns")
+RW_BANK_DIR = Path("bank")
+RW_SOLUTIONS_DIR = Path("solns")
+MATH_BANK_DIR = RW_BANK_DIR / "math"
+MATH_SOLUTIONS_DIR = RW_SOLUTIONS_DIR / "math"
 GENERATED_DIR = Path("generated")
 
-COMBINED_QUESTIONS_PATH = BANK_DIR / "combined_questions.pdf"
-COMBINED_SOLUTIONS_PATH = SOLUTIONS_DIR / "combined_answers.pdf"
+COMBINED_QUESTIONS_PATH = RW_BANK_DIR / "combined_questions.pdf"
+COMBINED_SOLUTIONS_PATH = RW_SOLUTIONS_DIR / "combined_answers.pdf"
+MATH_COMBINED_QUESTIONS_PATH = MATH_BANK_DIR / "math_combined_questions.pdf"
+MATH_COMBINED_SOLUTIONS_PATH = MATH_SOLUTIONS_DIR / "math_combined_solutions.pdf"
 
 CATALOG_PATH = GENERATED_DIR / "FIXED_catalog.json"
 CATALOG_REPORT_PATH = GENERATED_DIR / "catalog_blank_results.txt"
 
 learning_area_key = {"LA1": ["Craft and Structure", []], "LA2": ["Information and Ideas", []],
                      "LA3": ["Standard English Conventions", []], "LA4": ["Expression of Ideas", []]}
+
 difficulty_levels = ['Easy', 'Medium', 'Hard']
 
